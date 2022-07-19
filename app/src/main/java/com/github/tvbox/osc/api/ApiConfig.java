dea7ceb04401630f656d70372bf412ee8de14402
Damonwei1120 <93513840+Damonwei1120@users.noreply.github.com>
2022-07-19 08:56:47 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -79,7 +79,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://84k.cn/weitv");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://84k.cn/dweitv");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
