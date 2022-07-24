13e35cbc9b65deb424b7e2f06f8adf577d3db5c6
Damonwei1120 <93513840+Damonwei1120@users.noreply.github.com>
2022-07-24 10:58:41 +0800

    Update PlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
@@ -349,10 +349,10 @@ public class PlayActivity extends BaseActivity {
                 mVodPlayerCfg.put("sp", 1.0f);
             }
             if (!mVodPlayerCfg.has("st")) {
-                mVodPlayerCfg.put("st", 0);
+                mVodPlayerCfg.put("st", 60);
             }
             if (!mVodPlayerCfg.has("et")) {
-                mVodPlayerCfg.put("et", 0);
+                mVodPlayerCfg.put("et", 60);
             }
         } catch (Throwable th) {
 
