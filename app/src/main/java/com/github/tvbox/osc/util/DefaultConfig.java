97d5aca2b3538b8ccccc265c9fccf73bcfe2a028
Damonwei1120 <93513840+Damonwei1120@users.noreply.github.com>
2022-07-25 15:12:04 +0800

    Update DefaultConfig.java

--- app/src/main/java/com/github/tvbox/osc/util/DefaultConfig.java
+++ app/src/main/java/com/github/tvbox/osc/util/DefaultConfig.java
@@ -48,7 +48,7 @@ public class DefaultConfig {
             }
         }
         if (withMy)
-            data.add(0, new MovieSort.SortData("my0", "直播&设置"));
+            data.add(0, new MovieSort.SortData("my0", "首页&直播"));
         Collections.sort(data);
         return data;
     }
