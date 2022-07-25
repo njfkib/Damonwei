0f776b5ad8a6111335f978d6a2796b8453c96c3f
Damonwei1120 <93513840+Damonwei1120@users.noreply.github.com>
2022-07-25 15:49:23 +0800

    Update DetailActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
@@ -277,8 +277,7 @@ public class DetailActivity extends BaseActivity {
                     seriesAdapter.getData().get(vodInfo.playIndex).selected = true;
                     seriesAdapter.notifyItemChanged(vodInfo.playIndex);
                     jumpToPlay();
-                    if (showPreview && !fullWindows)
-                        toggleFullPreview();
+
                 }
             }
         });
