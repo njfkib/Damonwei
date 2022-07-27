adcd9d54f45908250855e9002d70301556a5e7b3
Damonwei1120 <93513840+Damonwei1120@users.noreply.github.com>
2022-07-27 10:09:54 +0800

    Update SourceViewModel.java

--- app/src/main/java/com/github/tvbox/osc/viewmodel/SourceViewModel.java
+++ app/src/main/java/com/github/tvbox/osc/viewmodel/SourceViewModel.java
@@ -604,13 +604,23 @@ public class SourceViewModel extends ViewModel {
                             str = new String[]{urlInfo.urls};
                         }
                         List<Movie.Video.UrlBean.UrlInfo.InfoBean> infoBeanList = new ArrayList<>();
+//                        for (String s : str) {
+//                            if (s.contains("$")) {
+//                                String[] ss = s.split("\\$");
+//                                if (ss.length >= 2) {
+//                                    infoBeanList.add(new Movie.Video.UrlBean.UrlInfo.InfoBean(ss[0], ss[1]));
+//                                }
+//                                //infoBeanList.add(new Movie.Video.UrlBean.UrlInfo.InfoBean(s.substring(0, s.indexOf("$")), s.substring(s.indexOf("$") + 1)));
+//                            }
+//                        }
                         for (String s : str) {
-                            if (s.contains("$")) {
-                                String[] ss = s.split("\\$");
-                                if (ss.length >= 2) {
+                            String[] ss = s.split("\\$");
+                            if (ss.length > 0) {
+                                if (ss.length == 2) {
                                     infoBeanList.add(new Movie.Video.UrlBean.UrlInfo.InfoBean(ss[0], ss[1]));
+                                } else if (ss.length == 1) {
+                                    infoBeanList.add(new Movie.Video.UrlBean.UrlInfo.InfoBean((infoBeanList.size() + 1) + "", ss[0]));
                                 }
-                                //infoBeanList.add(new Movie.Video.UrlBean.UrlInfo.InfoBean(s.substring(0, s.indexOf("$")), s.substring(s.indexOf("$") + 1)));
                             }
                         }
                         urlInfo.beanList = infoBeanList;
